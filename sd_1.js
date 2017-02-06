6b51d7c26b091e4aff9791b268288807c0512684
lubieciastka <jakub.chamicewicz@gmail.com>
2017-02-06 19:00:05 +0100

    Add files via upload

---
+++ sd_1.js
@@ -0,0 +1,31 @@
+https://jsfiddle.net/
+
+https://github.com/lubieciastka
+
+
+var myArray = [];
+
+for(var i = 0; i < 101; i++){
+	//ciało funkcji - to co wywoluje sie w petli
+  myArray.push(i);
+}
+
+console.log(myArray);
+
+var myArray2 = [];
+
+// for with modulo
+
+for(var i = 0; i < 101; i++){
+	if(i % 2 === 0){
+  	myArray2.push(i);
+  }	
+}
+
+console.log(myArray2);
+
+
+
+
+
+
