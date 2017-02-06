f4bcf299eb12ea74915672d1ac82ba2375a9c382
lubieciastka <jakub.chamicewicz@gmail.com>
2017-02-06 19:37:19 +0100

    Add files via upload

--- sd_1.js
+++ sd_1.js
@@ -1,7 +1,7 @@
 https://jsfiddle.net/
 
 https://github.com/lubieciastka
-
+https://github.com/lubieciastka/dotnet
 
 var myArray = [];
 
@@ -23,6 +23,33 @@ for(var i = 0; i < 101; i++){
 }
 
 console.log(myArray2);
+
+
+//1. definiujemy zmienna z tablica
+//2. tworzymy petle for - 10 iteracji
+//3. w petli tworzymy zmienna tymczasowa z druga tablica (rzedy)
+//4. robi drugiego fora (iterator nazywa inaczej niz w petli wyzej )
+//5. w drugiej petli - 10 iteracji
+//6. w drugiej petli wrzucamy do tymczasowej tablicy reszte z dzielenia
+//7. konczymy druga petle
+//8. zmienna tymczasowa (juz wypelniona) wrzucamy do pierwszej tablicy
+
+
+var myArray = [];
+
+for(var i = 0; i < 10; i++){
+	var myArrayTempRow = [];
+  
+	for(var j = 0; j < 10; j++){
+  	myArrayTempRow.push((i + j) % 2);
+  }
+  
+  myArray.push(myArrayTempRow);
+}
+
+console.table(myArray);
+
+
 
 
 
