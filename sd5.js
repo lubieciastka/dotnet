93e0e55d2a7265b32d6273540d8939a94404c6de
lubieciastka <jakub.chamicewicz@gmail.com>
2017-02-11 10:21:53 +0100

    Add files via upload

--- sd5.js
+++ sd5.js
@@ -1,6 +1,6 @@
 //funkcje zadanie 1
 //Funkcje, zadanie 1: Stworz funkcje ktora wypisze 3 najwieksze elementy w tablicy
- 
+
 var table = [1, 4, 232, 2, 10, 123123, 22];
 
 function findTopThree(arrayToSearch) {
@@ -12,3 +12,34 @@ function findTopThree(arrayToSearch) {
 }
 
 console.log(findTopThree(table));
+// funkcje zadanie2
+// Stworz skrypt do zbudowania tablicy do gry statki w wersji HTML,
+// https://jsfiddle.net/8tqjqa7f/3/
+// <div id="statki"></div>
+
+var statki = document.querySelector('#statki');
+
+draw(10);
+
+function draw(size) {
+  statki.innerHTML = "";
+
+  var table = statki.appendChild(document.createElement('table'));
+
+  for (var i = 0; i < size; i++) {
+    table.appendChild(drawRow(size));
+  }
+}
+
+function drawRow(size) {
+  var tr = document.createElement('tr');
+
+  for (var i = 0; i < size; i++) {
+    var td = document.createElement('td');
+    td.innerHTML = "X";
+    tr.appendChild(td);
+  }
+
+  return tr;
+}
+
