094a1d97b5a4a6dfa84bf39d8f8be90f7b95302c
lubieciastka <jakub.chamicewicz@gmail.com>
2017-02-11 13:38:07 +0100

    Add files via upload

--- sd5.js
+++ sd5.js
@@ -60,3 +60,78 @@ image.addEventListener('click', function(){
 window.addEventListener('resize', function(event){
 	console.log(window.outerHeight, window.outerWidth);
 });
+
+
+// statki - wersja z obrazkami
+//<div id="statki"></div>
+// table {
+//   color: white;
+//   border-collapse: collapse;
+// }
+
+// td {
+//   background: #666;
+//   width: 20px;
+//   height: 20px;
+// }
+
+// td img{
+//   max-width:30px;
+// }
+
+// .water {
+//   background-color: blue;
+// }
+
+var statki = document.querySelector('#statki');
+var game = [];
+var pirateImg = "<img src='https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/pirates_ship.png'/>";
+
+draw(generate(game, 10));
+
+function generate(gameTable, size) {
+  for (var i = 0; i < size; i++) {
+    var tempRow = [];
+    for (var j = 0; j < size; j++) {
+      if (Math.random() < 0.2) {
+        tempRow.push(1);
+      } else {
+        tempRow.push(0);
+      }
+    }
+    gameTable.push(tempRow);
+  }
+
+  return gameTable;
+}
+
+function draw(gameTable) {
+  statki.innerHTML = "";
+
+  var table = statki.appendChild(document.createElement('table'));
+
+  for (var i = 0; i < gameTable.length; i++) {
+    table.appendChild(drawRow(gameTable[i]));
+  }
+
+  function drawRow(row) {
+    var tr = document.createElement('tr');
+
+    for (var i = 0; i < row.length; i++) {
+      var td = document.createElement('td');
+      if (row[i] === 1) {
+        td.innerHTML = pirateImg;
+      } else {
+        td.classList.add('water');
+        td.innerHTML = "&nbsp;";
+      }
+      tr.appendChild(td);
+    }
+
+    return tr;
+  }
+}
+
+
+
+
